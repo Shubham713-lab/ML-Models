032bed9c8e1a5bf5d059f7d3a9a4fc730511e9b9
Shubham713-lab <shubham.chaubey124985@marwadiuniversity.ac.in>
2026-08-02 19:46:12 +0530

    web app for prediction

--- Diabetes_Prediction_ML/diabetics_webapp.py
+++ Diabetes_Prediction_ML/diabetics_webapp.py
@@ -3,7 +3,7 @@ import pickle
 import streamlit as st
 
 # loading the saved model
-loaded_model = pickle.load(open('/home/shubham/python-projects/Diabetes_Prediction_ML/trained_model.sav', 'rb'))
+loaded_model = pickle.load(open('trained_model.sav', 'rb'))
 
 # creating function for prediction
 def diabetes_prediction(input_data):
